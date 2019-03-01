3bd5615eb47d5b34e47b35a8de2207401b972039
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2019-03-01 15:28:01 +0000

    create prey pred data with random initialization

--- TCN/prey_pred_model/prey_pred_data.py
+++ TCN/prey_pred_model/prey_pred_data.py
@@ -49,9 +49,30 @@ def prey_pred_data_init(examples, seq_length, yinit):
     for i in range(examples):
         stack_data[i,0,:] = y0[i:i+(seq_length + 1)]
         stack_data[i,1,:] = y1[i:i+(seq_length + 1)]
-    X = stack_data[:,:,:-1]
-    Y = stack_data[:,:,-1]
+    # X = stack_data[:,:,:-1]
+    # Y = stack_data[:,:,-1]
+    return stack_data
+
+def prey_pred_data_randinit(examples, seq_length):
+    batch_no = 10
+    batch_samples = m.floor(examples / batch_no)
+    full_data = torch.zeros((examples, 2, seq_length+1))
+    a = 50
+    b = 100
+    for i in range(batch_no):
+        yinit = [a + (b-a)*torch.rand(1) , a + (b-a)*torch.rand(1)]
+        batch_data = prey_pred_data_init(batch_samples, seq_length, yinit)
+        full_data[ batch_samples*i:(batch_samples*(i+1)) ] = batch_data
+    if examples%batch_no:
+        yinit = [a + (b-a)*torch.rand(1) , a + (b-a)*torch.rand(1)]
+        batch_data = prey_pred_data_init( (examples%batch_no) , seq_length, yinit)
+        full_data[ (batch_no*(i+1)): ] = batch_data
+    X = full_data[:,:,:-1]
+    Y = full_data[:,:,-1]
     return X,Y
+    
+
+
 
 # def prey_pred_data_randinit(examples, seq_length):
 #     N = examples + seq_length + 1  # Number of data points

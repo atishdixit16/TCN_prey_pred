66d55c587937d4086538bdfbba078b665f306b7d
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2019-03-07 11:42:52 +0000

    create dense NN model for prey pred sequence data

--- TCN/prey_pred_model/dense_model.py
+++ TCN/prey_pred_model/dense_model.py
@@ -1,14 +1,15 @@
 import torch
+from torch.nn import functional as F
 
 class DenseNN(nn.Module):
     def __init__(self, seq_length):
         super(DenseNN, self).__init__()
-
+        
         self.fc1 = nn.Linear(seq_length*2, int(seq_length*1.5) )
         self.fc2 = nn.Linear(int(seq_length*1.5), int(seq_length*0.5)
         self.fc3 = nn.Linear(int(seq_length*0.5), 2)
 
     def forward(self, x):
-        h1 = self.fc1(x.view(-1, seq_length*2))
-
-        return self.decode(z), z
+        h1 = F.relu ( self.fc1(x.view(-1, seq_length*2)) )
+        h2 = F.sigmoid (self.fc2(h1))
+        return self.fc3(h2)

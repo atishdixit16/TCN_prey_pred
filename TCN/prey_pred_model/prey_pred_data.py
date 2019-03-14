00b8b9ecd2cd333654b4f18873c7605cada8d3ca
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2019-03-14 15:26:30 +0000

    change prey_pred_data_randinit function

--- TCN/prey_pred_model/prey_pred_data.py
+++ TCN/prey_pred_model/prey_pred_data.py
@@ -25,47 +25,26 @@ def prey_pred_data(examples, seq_length, yinit = [80,50], t_range = (0.0, 200)):
     y = stack_data[:, :, -1]
     return X, y
 
-def prey_pred_data_init(examples, seq_length, yinit):
-    N = 3000  # Number of data points
-    if N < (examples+seq_length+1):
-        print('No of examples should be smaller than 3000-seq_length-1')
-        return
-    tmin = 0.0     # starting t value
-    tmax = 200.0   # final t value
-    t = np.linspace(tmin, tmax, N)   # time grid
-    ysol = odeint(dfunc, yinit, t)
-    stack_data = torch.zeros(examples, 2, seq_length+1)
-    y0 = torch.from_numpy(ysol[:, 0])
-    y1 = torch.from_numpy(ysol[:, 1])
-    for i in range(examples):
-        stack_data[i, 0, :] = y0[i:i+(seq_length + 1)]
-        stack_data[i, 1, :] = y1[i:i+(seq_length + 1)]
-    # X = stack_data[:,:,:-1]
-    # Y = stack_data[:,:,-1]
-    return stack_data
-
-
 def prey_pred_data_randinit(examples, seq_length):
     batch_no = 100
     batch_samples = m.floor(examples / batch_no)
-    full_data = torch.zeros((examples, 2, seq_length+1))
-    a = 50
-    b = 100
+    full_data = np.zeros((examples, 2, seq_length+1))
+    u_min = 50
+    u_max = 100
     for i in range(batch_no):
-        yinit = [a + (b-a)*torch.rand(1), a + (b-a)*torch.rand(1)]
-        batch_data = prey_pred_data_init(batch_samples, seq_length, yinit)
+        yinit = [u_min + (u_max-u_min)*torch.rand(1), u_min + (u_max-u_min)*torch.rand(1)]
+        batch_data = prey_pred_data(batch_samples, seq_length, yinit = yinit)
+        batch_data = np.concatenate( ( batch_data[0], batch_data[1].reshape(batch_samples, 2 , 1) ) , axis=2 )
         full_data[batch_samples*i:(batch_samples*(i+1))] = batch_data
     if examples % batch_no:
-        yinit = [a + (b-a)*torch.rand(1), a + (b-a)*torch.rand(1)]
-        batch_data = prey_pred_data_init(
-            (examples % batch_no), seq_length, yinit)
+        batch_data = prey_pred_data(
+            (examples % batch_no), seq_length, yinit = yinit)
+        batch_data = np.concatenate( ( batch_data[0], batch_data[1].reshape(batch_samples, 2 , 1) ) , axis=2 )
         full_data[(batch_no*(i+1)):] = batch_data
-    full_data = full_data[torch.randperm(examples)]
+    full_data = full_data[np.random.permutation(examples)]
     X = full_data[:, :, :-1]
-    Y = full_data[:, :, -1]
-    return X, Y
-
-
+    y = full_data[:, :, -1]
+    return X, y
 
 def prey_pred_data_init_withdt(examples, seq_length, yinit):
     N = 3000  # Number of data points

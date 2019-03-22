c0440138a016705d264f6973c698ae9d12f4a776
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2019-03-22 15:34:19 +0000

    remove model initialization

--- TCN/prey_pred_model/dense_model.py
+++ TCN/prey_pred_model/dense_model.py
@@ -14,20 +14,3 @@ class DenseNN(nn.Module):
         h1 = F.relu( self.fc1(x.contiguous().view(-1, seq_length*2)) )
         h2 = F.relu(self.fc2(h1))
         return self.fc3(h2)
-
-class DenseNN_init(nn.Module):
-    def __init__(self, seq_length):
-        super(DenseNN_init, self).__init__()
-        
-        self.fc1 = nn.Linear(seq_length*2, int(seq_length*1.5) )
-        self.fc2 = nn.Linear(int(seq_length*1.5), int(seq_length*0.5))
-        self.fc3 = nn.Linear(int(seq_length*0.5), 2)
-        with torch.no_grad():
-            self.fc1.weight = torch.nn.Parameter( torch.load('fc1.pt') )
-            self.fc2.weight = torch.nn.Parameter( torch.load('fc2.pt') )
-            self.fc3.weight = torch.nn.Parameter( torch.load('fc3.pt') )
-
-    def forward(self, x, seq_length):
-        h1 = F.relu( self.fc1(x.view(-1, seq_length*2)) )
-        h2 = F.relu(self.fc2(h1))
-        return self.fc3(h2)
